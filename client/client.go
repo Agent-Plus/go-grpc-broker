c0da0acb8109ad5d23aa7bd9a0a3fdcd623e841d
Pavel Rezunenko <paulrez@gmail.com>
2021-05-17 14:43:23 +0300

    Client

--- client/client.go
+++ client/client.go
@@ -10,9 +10,11 @@ import (
 )
 
 type ExchangeClient struct {
-	address string
-	api     api.ExchangeClient
-	token   string
+	address    string
+	api        api.ExchangeClient
+	consumers  []streamWorker
+	subscribed string
+	token      string
 }
 
 func New(addr string) (*ExchangeClient, error) {
@@ -27,6 +29,13 @@ func New(addr string) (*ExchangeClient, error) {
 	}, nil
 }
 
+func (ec *ExchangeClient) metadata() context.Context {
+	return metadata.AppendToOutgoingContext(
+		context.Background(),
+		"authorization", "Bearer "+ec.token,
+	)
+}
+
 func (ec *ExchangeClient) Authenticate(name, secret string) error {
 	tk, err := ec.api.Authenticate(context.Background(), &api.Identity{Id: name, Secret: secret})
 	if err != nil {
@@ -38,48 +47,125 @@ func (ec *ExchangeClient) Authenticate(name, secret string) error {
 }
 
 func (ec *ExchangeClient) Subscribe(name string, exc bool) error {
-	ctx := metadata.AppendToOutgoingContext(
-		context.Background(),
-		"authorization", "Bearer "+ec.token,
-	)
-
+	ctx := ec.metadata()
 	_, err := ec.api.Subscribe(ctx, &api.SubscribeRequest{
 		Name:      name,
 		Tag:       "",
 		Exclusive: exc,
 	})
-	return err
-}
-
-type Deliverer interface {
-	Body() []byte
-}
-
-type message struct {
-	*api.Message
-}
-
-func (m *message) Body() []byte {
-	m.GetBody()
-}
-
-func (ec *ExchangeClient) Consume(m chan<- Deliverer) error {
-	ctx := metadata.AppendToOutgoingContext(
-		context.Background(),
-		"authorization", "Bearer "+ec.token,
-	)
-
-	stream, err := ec.api.Consume(ctx, &empty.Empty{})
 	if err != nil {
 		return err
 	}
 
-	for {
-		msg, err := stream.Recv()
-		if err != nil {
-			return err
-		}
+	ec.subscribed = name
+	return nil
+}
 
-		m <- &message{msg}
+type Message struct {
+	Body        []byte
+	ContentType string
+	CorId       string
+	Headers     map[string]string
+	Id          string
+}
+
+type streamWorker struct {
+	close   chan struct{}
+	deliver chan *Message
+}
+
+func (ec *ExchangeClient) Consume() (<-chan *Message, error) {
+	ctx := ec.metadata()
+	stream, err := ec.api.Consume(ctx, &empty.Empty{})
+	if err != nil {
+		return nil, err
 	}
+
+	if header, err := stream.Header(); err != nil {
+		return nil, err
+	} else {
+		if v := header.Get("x-state"); len(v) > 0 && v[0] == "established" {
+			//t.Log("x-state:", v)
+		}
+	}
+
+	resp := make(chan *Message)
+
+	go readStrem(stream, &streamWorker{
+		close:   make(chan struct{}),
+		deliver: resp,
+	})
+
+	return resp, nil
+}
+
+func readStrem(stream api.Exchange_ConsumeClient, worker *streamWorker) {
+	for {
+		select {
+		case <-worker.close:
+			return
+
+		default:
+			msg, err := stream.Recv()
+			if err != nil {
+				panic(err)
+			}
+
+			m := &Message{
+				ContentType: msg.ContentType,
+				CorId:       msg.CorId,
+				Id:          msg.Id,
+			}
+
+			if ln := len(msg.Body); ln > 0 {
+				m.Body = make([]byte, ln)
+				copy(m.Body, msg.Body)
+			} else {
+				m.ContentType = ""
+			}
+
+			if ln := len(msg.Headers); ln > 0 {
+				m.Headers = make(map[string]string, ln)
+				for k, v := range msg.Headers {
+					m.Headers[k] = v
+				}
+			}
+
+			worker.deliver <- m
+		}
+	}
+}
+
+func (ec *ExchangeClient) Publish(topic string, msg Message) error {
+	ctx := ec.metadata()
+
+	m := &api.Message{
+		ContentType: msg.ContentType,
+		CorId:       msg.CorId,
+		Id:          msg.Id,
+	}
+
+	if ln := len(msg.Body); ln > 0 {
+		m.Body = make([]byte, ln)
+		copy(m.Body, msg.Body)
+	} else {
+		m.ContentType = ""
+	}
+
+	if ln := len(msg.Headers); ln > 0 {
+		m.Headers = make(map[string]string, ln)
+		for k, v := range msg.Headers {
+			m.Headers[k] = v
+		}
+	}
+
+	_, err := ec.api.Publish(ctx, &api.PublishRequest{
+		Topic:   topic,
+		Message: m,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
